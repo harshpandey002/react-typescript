70416b2429afa429eebda66eccb807114423dfc2
Harsh Kumar Pandey <coding.harshp@gmail.com>
2022-01-02 22:05:57 +0530

    Types

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,7 +1,17 @@
-export const Greet = () => {
+type GreetProps = {
+  name: string;
+  messageCount: number;
+  isLoggedIn: boolean;
+};
+
+export const Greet = (props: GreetProps) => {
   return (
     <div>
-      <h2>Welcome Harsh! you have 10 unread messages</h2>
+      <h2>
+        {props.isLoggedIn
+          ? `Welcome {props.name}! you have {props.messageCount} unread messages`
+          : "Welcome Guest"}
+      </h2>
     </div>
   );
 };
