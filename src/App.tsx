1b055b4d68159fa072c966fc112158a7158fee1b
Harsh Kumar Pandey <coding.harshp@gmail.com>
2022-01-03 20:19:24 +0530

    Click event as props

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import { Heading } from "./components/Heading";
-import { Oscar } from "./components/Oscar";
-import { Status } from "./components/Status";
+import { Button } from "./components/Button";
 
 function App() {
   return (
     <div className="App">
-      <Status status="loading" />
-      <Heading>Placeholder text</Heading>
-
-      <Oscar>
-        <Heading>Oscar foes to Leonardo Dicpario!</Heading>
-      </Oscar>
+      <Button
+        handleClick={(e, id) => {
+          console.log("Button Clicked", e, id);
+        }}
+      />
     </div>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+type ButtonProps = {
+  handleClick: (event: React.MouseEvent<HTMLButtonElement>, id: number) => void;
+};
+
+export const Button = (props: ButtonProps) => {
+  return <button onClick={(e) => props.handleClick(e, 1)}>Click</button>;
+};
