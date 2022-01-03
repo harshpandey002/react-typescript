c60c14acf0e77ace3781ba96c21f8f980517c338
Harsh Kumar Pandey <coding.harshp@gmail.com>
2022-01-03 20:09:31 +0530

    advance props staus

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import "./App.css";
 import { Heading } from "./components/Heading";
 import { Oscar } from "./components/Oscar";
+import { Status } from "./components/Status";
 
 function App() {
   return (
     <div className="App">
+      <Status status="loading" />
       <Heading>Placeholder text</Heading>
 
       <Oscar>
